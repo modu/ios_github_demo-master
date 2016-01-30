74a410be24b766d8b1d37c2800e679fe5ba06f42
varun vyas <moduvyas@gmail.com>
2016-01-29 23:59:05 -0500

    First commit

---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController {
+
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    private func doSearch() {
+
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+
+            // Print the returned repositories to the output window
+            for repo in newRepos {
+                print(repo)
+            }
+            self.repos = newRepos
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error)
+        })
+    }
+}
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true;
+    }
+
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true;
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
